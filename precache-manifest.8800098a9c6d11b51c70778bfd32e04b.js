65519740ce003cdb30804feb636627ba056b9315
zeleke <zzdd9251@gmail.com>
2020-04-01 13:00:21 -0700

    Updates

---
+++ precache-manifest.8800098a9c6d11b51c70778bfd32e04b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc2687e9ddd1e339a4f7db1d369592a9",
+    "url": "/todoreact/index.html"
+  },
+  {
+    "revision": "3c736dceaa17a89cbe0f",
+    "url": "/todoreact/static/css/main.db2f91af.chunk.css"
+  },
+  {
+    "revision": "4fca89b019c0ed9d90c8",
+    "url": "/todoreact/static/js/2.df09107f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/todoreact/static/js/2.df09107f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3c736dceaa17a89cbe0f",
+    "url": "/todoreact/static/js/main.94838f3e.chunk.js"
+  },
+  {
+    "revision": "6f491eb43618bf4151d8",
+    "url": "/todoreact/static/js/runtime-main.37207784.js"
+  }
+]);
